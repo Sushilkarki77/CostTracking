1e0e18487461f756093f80d475f259de957a412a
Sushil <sushilkarki352@gmail.com>
2025-10-28 23:18:12 -0500

    Frontend: UI color and sidebar items

--- Frontend/src/app/dashboard/layout/sidebar/sidebar.ts
+++ Frontend/src/app/dashboard/layout/sidebar/sidebar.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, model, ModelSignal, signal } from '@angular/core';
+import { LucideAngularModule, ChevronLeft, ChevronRight, DollarSign, ChartBar, LayoutDashboard } from 'lucide-angular';
+
+
+
+
 
 @Component({
   selector: 'dashboard-sidebar',
-  imports: [],
+  imports: [LucideAngularModule, CommonModule],
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.css',
+  styleUrl: './sidebar.scss',
 })
 export class Sidebar {
+ 
+   readonly chevronLeft = ChevronLeft;
+   readonly chevronRight = ChevronRight;
+
+
+    sideBarItems = [
+    { label: 'Dashboard', icon: LayoutDashboard, size: 36, text: '', isHeader: true },
+    { label: 'Analytics', icon: ChartBar, size: 18, text: 'Analytics', isHeader: false },
+    { label: 'Expenses', icon: DollarSign, size: 18, text: 'Expenses', isHeader: false },
+  ];
+
+
+    expandedState = signal<boolean>(true);
+
+
+   handleExpandCollapse() {
+    this.expandedState.update(prev => !prev);
+   }
 
 }
