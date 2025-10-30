8f3417dd348a9f22a5ec7454102159e782736869
Sushil <sushilkarki352@gmail.com>
2025-10-30 00:51:03 -0500

    Frontend: Login, Register UI

--- Frontend/src/app/auth/login/login.ts
+++ Frontend/src/app/auth/login/login.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ArrowRight } from 'lucide-angular';
+import { LucideAngularModule } from 'lucide-angular/src/icons';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+
+  readonly chevronRight = ArrowRight;
+
+  private router = inject(Router);
+
+  navigateToRegister() {
+    this.router.navigate(['/auth/register']);
+  }
 
 }
--- Frontend/src/app/auth/register/register.ts
+++ Frontend/src/app/auth/register/register.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ArrowLeft, ArrowRight } from 'lucide-angular';
+import { ChevronLeft, LucideAngularModule } from 'lucide-angular/src/icons';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+
+  readonly chevronLeft = ArrowLeft;
+
+  private router = inject(Router)
+
+  navigateToLogin(){
+    this.router.navigate(['/auth/login'])
+  }
 
 }
