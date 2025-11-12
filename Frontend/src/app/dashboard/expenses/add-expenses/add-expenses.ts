d54d598413e94878a029c52707a93d66f8475493
Sushil <sushilkarki352@gmail.com>
2025-11-12 13:50:49 -0600

    Frontend: Expense component refactor

--- Frontend/src/app/dashboard/expenses/add-expenses/add-expenses.ts
+++ Frontend/src/app/dashboard/expenses/add-expenses/add-expenses.ts
@@ -1,15 +1,14 @@
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators, ɵInternalFormsSharedModule } from '@angular/forms';
+import {  ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LucideAngularModule, Plus } from 'lucide-angular/src/icons';
 import { ExpenseForm } from '../../../common/components/expense-form/expense-form';
 import { Store } from '@ngrx/store';
-import { selectAllCategories, selectCategoryInitialized } from '../../../store/category/category.selectors';
-import { Subject, takeUntil } from 'rxjs';
+import { selectAllCategories } from '../../../store/category/category.selectors';
 import { Category, Expense } from '../../../common/interfaces/app.interface';
 import { loadCategories } from '../../../store/category/category.actions';
-import { addExpense, createExpense } from '../../../store/expenses/expenses.actions';
+import {  createExpense } from '../../../store/expenses/expenses.actions';
 
 @Component({
   selector: 'app-add-expenses',
@@ -19,34 +18,19 @@ import { addExpense, createExpense } from '../../../store/expenses/expenses.acti
 })
 export class AddExpenses {
   private store = inject(Store);
+  categories$ = this.store.select(selectAllCategories);
 
   categories: Array<Category> = [];
-
-  private componentDemounted = new Subject<boolean>();
-
-  categories$ = this.store.select(selectAllCategories)
-
-
   private router = inject(Router);
 
   constructor() {
     this.store.dispatch(loadCategories());
   }
 
-
-  handleBack() {
-
-    this.router.navigate(['/dashboard/expenses'])
-  }
-
-  ngOnDestroy(): void {
-    this.componentDemounted.next(true);
-  }
-
+  handleBack = () =>  this.router.navigate(['/dashboard/expenses']);
+  
   handleFormSubmitted(expense: Partial<Expense>) {
     this.store.dispatch(createExpense({ expense }));
     this. handleBack();
   }
-
-
 }
